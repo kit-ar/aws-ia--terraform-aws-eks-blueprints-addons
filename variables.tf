effa457dfa204659d5bd41a7937f8360e30fa69a
Joao C Costa <joao.costa@kit-ar.com>
2024-05-08 00:51:04 +0100

    WIP: initial version (not tested)

--- variables.tf
+++ variables.tf
@@ -146,13 +146,13 @@ variable "aws_efs_csi_driver" {
 # AWS FSx for OpenZFS
 ################################################################################
 
-variable "enable_aws_fsx_openzfs_csi_driver" {
+variable "enable_aws_fsx_zfs_csi_driver" {
   description = "Enable AWS FSx for OpenZFS CSI Driver add-on"
   type        = bool
   default     = false
 }
 
-variable "aws_fsx_openzfs_csi_driver" {
+variable "aws_fsx_zfs_csi_driver" {
   description = "FSx for OpenZFS CSI Driver add-on configuration values"
   type        = any
   default     = {}
